7f336b7ea5910f3fe48df0f0ab1d7601002e9df6
Dmitry Bychkov <yom.4100@gmail.com>
2022-11-28 23:47:12 +0300

    add some code

---
+++ src/main/java/ru/netology/domain/Manager.java
@@ -0,0 +1,39 @@
+package ru.netology.domain;
+
+import ru.netology.Product;
+import ru.netology.repository.Repository;
+
+public class Manager {
+    private Repository repo;
+
+    public Manager(Repository repo) {
+        this.repo = repo;
+    }
+
+    public void add(Product product) {
+        repo.save(product);
+    }
+
+    public void removeById(int id) {
+        repo.removeById(id);
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repo.findAll()) {
+            if (matches(product, text)) {
+                result = new Product[result.length + 1];
+                result[result.length - 1] = product;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
